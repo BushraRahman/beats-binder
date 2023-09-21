1cd21977c3f823f536c131d3acab7878af1414c5
Zidanni Clerigo <zclerigo40@stuy.edu>
2023-09-21 14:43:31 -0400

    games

--- homework/unit_1/00_views/games_catalogue/games_catalogue/games/views.py
+++ homework/unit_1/00_views/games_catalogue/games_catalogue/games/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
    #return HttpResponse("Hello, world. You're at the games index.")
-   return render(request, "index.html")
+   titlePage = "Games Index"
+   gamesList = [{"name" : "Furry Shades of Gray", "Franchise" : False}, {"name" : "Among Us", "Franchise" : False}, {"name" : "Elden Ring", "Franchise" : True}, {"name" : "Minecraft", "Franchise" : True}]
+   return render(request, "games/index.html", context = {'titlePage' : titlePage,
+                                                         'gamesList' : gamesList})
