e41b88e72f2bf178e981c07f8959d5be2929f30f
Zidanni Clerigo <zclerigo40@stuy.edu>
2023-11-17 11:43:45 -0500

    ajax

--- homework/unit_2/05_ajax/js/ajax.js
+++ homework/unit_2/05_ajax/js/ajax.js
@@ -28,18 +28,39 @@ const populateSelectCountries = function (data) {
 };
 
 
-selectCountriesContainer.onchange = function() {
-    info = document.querySelector(".countries_select");
-    let name, capital, language, population, currencies;
-    let flag = new Image();
-    for (const country of countries_list.sort((a, b) => a.name.common > b.name.common)) {   
-        if (country.name.common == info.value) {
-            console.log(country)
-            name = country.name.common;
-            capital = country.capital;
-            language = country.languages;
-            population = country.population;
-            currencies = country.currencies;
-            flag.src=`${country.flags.png}`;
-        }
-    }
+selectCountriesContainer.addEventListener("change", function() {
+    const selectedCountry = this.value;
+    const requestResult = new XMLHttpRequest();
+    requestResult.open("GET", `https://restcountries.com/v3.1/name/${selectedCountry}`);
+    requestResult.send();
+
+    requestResult.addEventListener("load", function() {
+        const countryInfo = JSON.parse(this.responseText)[0];
+        displayCountryInfo(countryInfo);
+    })
+    // let name, capital, language, population, currencies;
+    // let flag = new Image();
+    // for (const country of countries_list.sort((a, b) => a.name.common > b.name.common)) {   
+    //     if (country.name.common == info.value) {
+    //         console.log(country)
+    //         name = country.name.common;
+    //         capital = country.capital;
+    //         language = country.languages;
+    //         population = country.population;
+    //         currencies = country.currencies;
+    //         flag.src=`${country.flags.png}`;
+    //     }
+    // }
+});
+
+const displayCountryInfo = function (country) {
+    const countryInfoContainer = document.querySelector(".result");
+    countryInfoContainer.innerHTML = `
+        <h2>Name: ${country.name.common}</h2>
+        <p>Capital: ${country.capital[0]}</p>
+        <p>lanugage: ${Object.values(country.languages).join(', ')}</p>
+        <p>Population: ${country.population}</p>
+        <p>Currency: ${Object.values(country.currencies).map(cur => cur.name).join(', ')}</p>
+        <img src="${country.flags.png}">
+    `
+}
