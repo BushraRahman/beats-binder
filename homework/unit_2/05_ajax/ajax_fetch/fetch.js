2af31eebd9e39c0f5a115a3b0c81e401cdee634b
Zidanni Clerigo <zclerigo40@stuy.edu>
2023-11-20 14:25:26 -0500

    fetch fix

--- homework/unit_2/05_ajax/ajax_fetch/fetch.js
+++ homework/unit_2/05_ajax/ajax_fetch/fetch.js
@@ -1,21 +1,7 @@
+//worked with tyler chan (feeling sick, will fix this later)
+
 // Creating XMLHttpRequest object
 const request_countries = new XMLHttpRequest();
-
-// Open the given file
-request_countries.open( "GET", "https://restcountries.com/v3.1/all?fields=name,flag,capital,population,languages,population,currencies,flags");
-
-// Sending the request
-request_countries.send();
-let countries_list;
-
-// Creating a callback function
-request_countries.addEventListener("load", function () {
-    countries_list = JSON.parse(this.responseText);
-    populateSelectCountries(countries_list);
-});
-
-const selectCountriesContainer = document.querySelector(".countries_select");
-
 
 const populateSelectCountries = function (data) {
     for (const country of data.sort((a, b) => a.name.common > b.name.common)) {
@@ -26,14 +12,43 @@ const populateSelectCountries = function (data) {
 };
 
 
+// Open the given file
+fetch("https://restcountries.com/v3.1/all?fields=name,flag,capital,population,languages,population,currencies,flags")
+
+.then(function (response) {
+    return response.json();
+}).then(populateSelectCountries);
+// Sending the request
+let countries_list;
+
+const selectCountriesContainer = document.querySelector(".countries_select");
+
+
+
+
 selectCountriesContainer.addEventListener("change", function() {
     const selectedCountry = this.value;
-    fetch(`https://restcountries.com/v3.1/name/${selectedCountry}`)
+    const requestResult = new XMLHttpRequest();
+    requestResult.open("GET", `https://restcountries.com/v3.1/name/${selectedCountry}`);
+    requestResult.send();
 
-    .then(function(response) {
-        const countryInfo = response.json;
+    requestResult.addEventListener("load", function() {
+        const countryInfo = JSON.parse(this.responseText)[0];
         displayCountryInfo(countryInfo);
     })
+    // let name, capital, language, population, currencies;
+    // let flag = new Image();
+    // for (const country of countries_list.sort((a, b) => a.name.common > b.name.common)) {   
+    //     if (country.name.common == info.value) {
+    //         console.log(country)
+    //         name = country.name.common;
+    //         capital = country.capital;
+    //         language = country.languages;
+    //         population = country.population;
+    //         currencies = country.currencies;
+    //         flag.src=`${country.flags.png}`;
+    //     }
+    // }
 });
 
 const displayCountryInfo = function (country) {
