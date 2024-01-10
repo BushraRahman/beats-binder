a2f4b1e6b3e8dce5ab7c217e5a20416c59bb6335
Bushra Rahman <agario4ev@gmail.com>
2024-01-10 14:49:37 -0500

    edited settings to add things needed for pgadmin

--- moron_the_worlds_smartest_lyrics_app/moron_the_worlds_smartest_lyrics_app/settings.py
+++ moron_the_worlds_smartest_lyrics_app/moron_the_worlds_smartest_lyrics_app/settings.py
@@ -9,11 +9,25 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# JSON-based secrets module
+with open(os.path.join(
+    BASE_DIR, moron_the_worlds_smartest_lyrics_app, 'secrets.json')) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+'''Get the secret variable or return explicit exception.'''
+    try:
+        return secrets[setting]
+        except KeyError:
+        error_msg = 'Set the {0} environment variable'.format(setting)
+        raise ImproperlyConfigured(error_msg)
 
 
 # Quick-start development settings - unsuitable for production
