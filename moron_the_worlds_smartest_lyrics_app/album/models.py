6198dc39b756ca1fda81b55ddf103901e8aadb8a
Hui Wen Weng <hweng40@stuy.edu>
2024-01-12 11:52:06 -0500

    updated models.pys

--- moron_the_worlds_smartest_lyrics_app/album/models.py
+++ moron_the_worlds_smartest_lyrics_app/album/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from artist.models import Artist
+from song.models import Song
 
 # Create your models here.
+
+class Album(models.Model): 
+    deezer_id = models.IntegerField(unique=True, null=False)
+    title = models.CharField(max_length=255, unique=True, null=False)
+    release_date = models.DateField()
+    cover = models.ImageField(unique=True, null=False)
+    genre = models.IntegerField(unique=True, null=False)
+    tracklist = models.URLField(unique=True, null=False)
+    album_artist = models.ManyToManyField(Artist)
--- moron_the_worlds_smartest_lyrics_app/song/models.py
+++ moron_the_worlds_smartest_lyrics_app/song/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from artist.models import Artist
+from album.models import Album
 
 # Create your models here.
+
+class Song(models.Model): 
+    deezer_id = models.IntegerField(unique=True, null=False)
+    title = models.CharField(max_length=255, unique=True, null=False)
+    release_date = models.DateField()
+    preview = models.URLField(unique=True, null=False)
+    song_artist = models.ManyToManyField(Artist)
+    song_album = models.ManyToManyField(Album)
