cdf774c4da548922a925e45aaa008bd747363b7c
Bushra Rahman <agario4ev@gmail.com>
2024-01-14 15:01:22 -0500

    created models for albums, artists, and songs

--- Beats_Binder/artists/models.py
+++ Beats_Binder/artists/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Artist(models.Model):
+	deezer_id = models.IntegerField()
+	name = models.CharField(max_length=100)
+	cover = models.URLField(max_length=200)
+	albums = models.ManyToManyField('albums.Album')
+	songs = models.ManyToManyField('songs.Song')
+	nb_album = models.IntegerField
+
+# Create your models here.
+
 # Create your models here.
--- Beats_Binder/songs/models.py
+++ Beats_Binder/songs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Song(models.Model):
+	deezer_id = models.IntegerField()
+	name = models.CharField(max_length=100)
+	albums = models.ManyToManyField('albums.Album')
+	artists = models.ManyToManyField('artists.Artist')
+	duration = models.IntegerField()
+	preview = models.URLField(max_length=200)
 # Create your models here.
