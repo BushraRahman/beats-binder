ba32f0a16ca4bcae5992c8e3285be50ea670ceea
Bushra Rahman <agario4ev@gmail.com>
2024-01-16 16:21:19 -0500

    edited models to resolve manytomanyfield conflicts, added (incredibly basic) list views that are the home pages of each app, added a base.html template in core bc that somehow got deleted

--- Beats_Binder/albums/views.py
+++ Beats_Binder/albums/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Album
+# Create your views here.
+
+class AlbumListView(ListView):
+	model = Album
+	
 
 # Create your views here.
--- Beats_Binder/songs/models.py
+++ Beats_Binder/songs/models.py
@@ -3,8 +3,6 @@ from django.db import models
 class Song(models.Model):
 	deezer_id = models.IntegerField()
 	name = models.CharField(max_length=100)
-	albums = models.ManyToManyField('albums.Album')
-	artists = models.ManyToManyField('artists.Artist')
 	duration = models.IntegerField()
 	preview = models.URLField(max_length=200)
 	saved = models.BooleanField(default=False)
