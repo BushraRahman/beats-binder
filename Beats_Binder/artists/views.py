cbe5837a48dec5e808c2c531a44cf51f65589700
Hui Wen Weng <hweng40@stuy.edu>
2024-01-22 14:17:58 -0500

    differentiate search functions

--- Beats_Binder/artists/views.py
+++ Beats_Binder/artists/views.py
@@ -5,14 +5,16 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from .models import Artist
-from .search_form import SearchForm
+from .artist_search_form import ArtistSearchForm
+import requests
+
 # Create your views here.
 
 class ArtistListView(ListView):
     model = Artist
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['search_form'] = SearchForm
+        context['artist_search_form'] = ArtistSearchForm
         return context
 
 class ArtistDetailView(DetailView):
@@ -20,20 +22,20 @@ class ArtistDetailView(DetailView):
 	
 def search_results_view(request):
     if request.method == "GET":
-        form = SearchForm(request.GET)
+        form = ArtistSearchForm(request.GET)
         if form.is_valid():
             search_input = form.cleaned_data["Search"]
             search_result = searchAPI(search_input)
-            return render(request, "artists/search_results.html", context={"search_result": search_result["data"],
+            return render(request, "artists/artist_search_results.html", context={"search_result": search_result["data"],
                                                                         	"search_input": search_input,
-                                                               "search_form": form})
+                                                                            "search_form": form})
     else: 
-        form = SearchForm()
-    return render(request, "artists/search_results.html", 
-            context={'search_form': SearchForm})
+        form = ArtistSearchForm()
+    return render(request, "artists/artist_search_results.html", 
+            context={'search_form': ArtistSearchForm})
     
 def searchAPI(search_input):
-    url = "https://deezerdevs-deezer.p.rapidapi.com/search/artists"
+    url = "https://deezerdevs-deezer.p.rapidapi.com/search"
     querystring = {"q": search_input}
     headers = {
         "X-RapidAPI-Key": "de8f6f2a3fmsh850207b34ede80bp17e3d8jsnd9883430d914",
--- Beats_Binder/songs/urls.py
+++ Beats_Binder/songs/urls.py
@@ -7,5 +7,5 @@ app_name = "songs"
 urlpatterns = [
   path("", views.SongListView.as_view(), name="list_view"),
   path("/<int:pk>", views.SongDetailView.as_view(), name="list_view"),
-  path("search_results", views.search_results_view, name="search_results_view"),
+  path("/song_search_results", views.search_results_view, name="search_results_view"),
 ]
