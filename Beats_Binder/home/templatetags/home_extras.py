58df8ec592a26da44512e1e0642720a8904b624a
Bushra Rahman <agario4ev@gmail.com>
2024-01-22 11:38:17 -0500

    added links in album list view that redirect to each album

--- Beats_Binder/home/templatetags/home_extras.py
+++ Beats_Binder/home/templatetags/home_extras.py
@@ -10,3 +10,7 @@ def concatenate(str1, str2):
 @register.filter
 def returnHomeButton(name, text):
     return "<button type='submit' name='" + str(name) +"'>"+str(text)+"</button>"
+
+@register.filter
+def returnLink(url,text):
+    return "<a href='" + url + "'>" + text + "</a>"
