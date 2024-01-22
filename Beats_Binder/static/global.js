315fd424f6f4a93f7973bcf9ca7dd2315f1ca055
zClerigo <124084016+zClerigo@users.noreply.github.com>
2024-01-21 22:49:22 -0500

    Update global.js

--- Beats_Binder/static/global.js
+++ Beats_Binder/static/global.js
@@ -9,14 +9,17 @@
   
   function themeToggle() {
     let element = document.body;
+    let navBar = document.querySelector(".navbar");
+
     element.classList.toggle("dark_mode");
-  
+    navBar.classList.toggle("navbar-dark");
+
     let theme = localStorage.getItem("theme");
     if (theme && theme === "dark_mode") {
-      localStorage.setItem("theme", "");
+        localStorage.setItem("theme", "");
     } else {
-      localStorage.setItem("theme", "dark_mode");
+        localStorage.setItem("theme", "dark_mode");
     }
-  
+}
+
     // document.getElementById("theme").textContent = localStorage.getItem("theme");
-  }
